630a72af11c7d7de619f80a7d61a348fec18e51e
VolodymyrPalahniuk <palagnyukvolodymyranatoliy@gmail.com>
2026-01-16 12:40:19 +0200

    Перша сторінка

--- form-site/src/pages/LoginPage/LoginPage.jsx
+++ form-site/src/pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import {
     Container,
     Box,
@@ -8,11 +8,11 @@ import {
 } from "@mui/material";
 
 const LoginPage = () => {
-    const [fromData, setFormData] = useState({
-        login: "",
-        password: ""
+    const [formData, setFormData] = useState({
+      login: '',
+      password: ''
     });
-    
+      
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData(prev => ({
@@ -20,3 +20,72 @@ const LoginPage = () => {
         [name]: value
         }));
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Данні входу ", fromData);
+        //НАЗІ СЮДИ ЛОГОКІ АВТОРИЗАЦІЇ
+    };
+
+    return (
+    <Container maxWidth="sm">
+      <Box sx={{ 
+        marginTop: 8, 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center' 
+      }}>
+        <Typography component="h1" variant="h5" align="center" gutterBottom>
+            Авторизація
+          </Typography>
+
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="login"
+              label="E-mail або номер телефону"
+              name="login"
+              autoComplete="email"
+              autoFocus
+              value={formData.login}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Пароль"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              УВІЙТИ
+            </Button>
+
+            <Button
+              fullWidth
+              variant="text"
+              sx={{ textTransform: 'none' }}
+              onClick={() => console.log('Перехід до створення без авторизації')}
+            >
+              Додати заявку без авторизації
+            </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginPage;
