e83081c3f17085e6247fe5b22e0ec2b1f2c40ccd
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2026-03-06 20:21:42 +0200

    Перенаправлення при відсутності токена

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -15,4 +15,18 @@ axiosClient.interceptors.request.use((config) => {
     return config;
 });
 
+axiosClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            window.location.href = '/tasks/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axiosClient;
